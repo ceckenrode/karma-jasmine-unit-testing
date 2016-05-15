26443117bbcd82564a1164ed8b3a8cc8483246cd
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-05-15 15:57:02 -0400

    added test for XHR request and response

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,8 @@
 var testingAngularApp = angular.module("testingAngularApp", []);
-testingAngularApp.controller("testingAngularCtrl", function($rootScope, $scope) {
+testingAngularApp.controller("testingAngularCtrl", function($rootScope, $scope, $http) {
   $scope.title = "testing angular js applications";
+
+  $scope.apiKey = "102daf972f87ab6d4788fac310d99567";
 
   $scope.destinations = [];
 
@@ -18,5 +20,26 @@ testingAngularApp.controller("testingAngularCtrl", function($rootScope, $scope) 
 
   $scope.removeDestination = function(index) {
     $scope.destinations.splice(index, 1);
+  };
+
+  $scope.getWeather = function(destination) {
+    $http.get("http://api.openweathermap.org/data/2.5/weather?q=" + destination.city + "&appid=" +
+      $scope.apiKey).then(
+      function successCallback(response) {
+        if (response.data.weather) {
+          destination.weather = {};
+          destination.weather.main = response.data.weather[0].main;
+          destination.weather.temp = $scope.convertKelvinToCelcius(response.data.main.temp);
+          console.log($scope.destinations);
+        }
+
+      },
+      function errorCallback(error) {
+        console.log(err);
+      });
+  };
+
+  $scope.convertKelvinToCelcius = function (temp) {
+    return Math.round(temp - 273);
   };
 });
--- app/js/test/unit/testingAngularUnitSpec.js
+++ app/js/test/unit/testingAngularUnitSpec.js
@@ -7,20 +7,26 @@ describe('Testing Angular Js Test Suite', function() {
   //we describe our controller test and pass a callback
   describe('Testing Angular Js Controller', function() {
     //we define the variables we will use globally
-    var scope, ctrl;
+    var scope, ctrl, httpBackend;
     //we for each test we run, we inject our scope and controller variables
     //scope becomes an angular $scope
     //ctrl becomes and angular controller
-
-    beforeEach(inject(function($controller, $rootScope) {
+    //httpBackend = $httpBackend
+    beforeEach(inject(function($controller, $rootScope, $httpBackend) {
       scope = $rootScope.$new();
       ctrl = $controller('testingAngularCtrl', {
         $scope: scope
       });
+      httpBackend = $httpBackend;
     }));
 
     afterEach(function() {
       //cleanUp code goes here if we need it
+
+      //we make sure there are no outstanding expectations for httpBackend
+      //verify there are no outstanding requests for httpbackend
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
     });
 
     //we define our test condition and pass a callback
@@ -77,6 +83,29 @@ describe('Testing Angular Js Test Suite', function() {
 
       expect(scope.destinations[0].city).toBe("Warsaw");
       expect(scope.destinations[0].country).toBe("Poland");
+    });
+
+    it('should update the weather for a specific destination', function(){
+      //setting up a starter destination
+      scope.destination = {
+        city: "Melbourne",
+        country: "Austrailia"
+      };
+
+      //mocking a get request. the respond is a placeholder response.data
+      httpBackend.expectGET("http://api.openweathermap.org/data/2.5/weather?q=" + scope.destination.city + "&appid=" +
+        scope.apiKey).respond({
+          weather: [{main: "Rain", detail: "Light rain"}],
+          main: {temp: 288}
+        });
+
+      //doing the mock request
+      scope.getWeather(scope.destination);
+
+      //must flush the request after making
+      httpBackend.flush();
+      expect(scope.destination.weather.main).toBe("Rain");
+      expect(scope.destination.weather.temp).toBe(15);
     });
   });
 });
